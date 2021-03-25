8769977e475cc8cab3d23e9e1c63af65c73aeac2
Shekhar Tyagi <notshekhar@gmail.com>
2021-03-25 09:11:58 +0530

    new

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ function gcd(a, b) {
     return gcd(b, r)
 }
 
+process.stdout.write("Calculating value of PI...\r")
 for (let i = 0; i < total; i++) {
-    //gcd == 1 both numbers are coprime
+    //gcd -> 1 both numbers are coprime
     let n1 = Math.floor(Math.random() * total) + 1
     let n2 = Math.floor(Math.random() * total) + 1
     if (gcd(n1, n2) == 1) coprimeCount++
 }
 // console.log(coprimeCount, total)
 let pi = Math.sqrt((6 * total) / coprimeCount)
-console.log("Final PI value is", pi)
+process.stdout.write("Final PI value is " + pi + "\r")
